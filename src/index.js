b18d176caa99eac6f0fd51d6a29fb306d1c8029b
Kaustubh <kkaustubh@us.ibm.com>
2019-01-26 09:08:54 -0800

    Added hot module replacement for component and reducer.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,30 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import configureStore from './app/store/configureStore';
 
 const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let render = () => {
+  const App = require('./App').default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  );
+};
+
+
+if (process.env.NODE_ENV !== 'production') {
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      setTimeout(render);
+    })
+  }
+}
+
+render();
