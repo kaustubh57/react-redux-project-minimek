741971f04729dab95a8a835ef035af1887e97b34
Kaustubh <kkaustubh@us.ibm.com>
2019-01-30 06:10:13 -0800

    Added tabsReducer to root reducer.

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import testReducer from './testReducer';
+import tabsReducer from '../../features/tabs/tabsReducer';
 
 const rootReducer = combineReducers({
-  test: testReducer
+  test: testReducer,
+  tabs: tabsReducer
 });
 
 export default rootReducer;
